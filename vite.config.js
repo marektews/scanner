e112edf69f24f14bc460d32ab353bf34d0224a87
Marek Tews <marek.tews@gmail.com>
2024-06-01 14:34:11 +0200

    Anulowanie zmian w konfiguracji

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    base: mode === 'production' ? '/scanner/' : '',
+    base: '',
     server: {
         proxy: {
             "/api": {
